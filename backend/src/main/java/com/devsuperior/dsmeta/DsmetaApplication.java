999dbb5ebfd0e601afaaa4ec6d4656429990bb44
Nelio Alves <acenelio@gmail.com>
2022-07-09 19:16:17 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsmeta/DsmetaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
