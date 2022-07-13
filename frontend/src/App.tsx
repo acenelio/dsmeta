a821c9f10d3396495f67c30520a28c6c1a47df80
Nelio Alves <acenelio@gmail.com>
2022-07-13 19:40:49 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./components/Header";
 import SalesCard from "./components/SalesCard";
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg';
 import { BASE_URL } from '../../utils/request';
 
@@ -11,7 +12,7 @@ type Props = {
 function handleClick(id :number) {
     axios(`${BASE_URL}/sales/${id}/notification`)
         .then(response => {
-            console.log("SUCESSO");
+            toast.info("SMS enviado com sucesso");
         });
 }
 
